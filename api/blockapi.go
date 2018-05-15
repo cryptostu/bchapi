e966c62ed486ed6be0ef9d8a350dd824fcc30af9
jjz <woaf1003@gmail.com>
2018-05-15 16:50:37 +0800

    add blcokapi_test

--- api/blockapi.go
+++ api/blockapi.go
@@ -1,6 +1,23 @@
 package api
 
+import (
+	"fmt"
+	"github.com/cryptostu/bchapi"
+	"github.com/cryptostu/bchapi/model"
+)
+
 const (
 	BLOCK_URL    = "block/%d/"
 	BLOCK_TX_URL = "block/%d/tx"
 )
+
+func GetBlockTxs(height int) (error, []*model.Tx) {
+	url := fmt.Sprintf(BLOCK_TX_URL, height)
+	result, err := bchapi.HttpGet(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	txs, err := model.StringToTxs(result)
+	return err, txs
+
+}
